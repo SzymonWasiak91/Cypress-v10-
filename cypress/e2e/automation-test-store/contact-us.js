ff200fbe38c91a05d8c5ac1705ef456c0571c18e
Szymon <wasiak.szymon.1991@gmail.com>
2022-11-27 16:30:52 +0100

    Add first assertions

--- cypress/e2e/automation-test-store/contact-us.js
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -1,20 +1,19 @@
 /// <reference types="Cypress" />
 
 describe("Automation test store",()=>{
-    it("Automation test for contact us form",()=>{
+    it.only("Automation test for contact us form",()=>{
         cy.visit("https://www.automationteststore.com/");
-        cy.get("a[href='https://automationteststore.com/index.php?rt=content/contact']").click();
+        //dodanie $ sprawia że szuka tekstu na końcu hrefa
+        cy.get("a[href$='contact']").click();
         cy.get('#ContactUsFrm_first_name').type("Szymon");
         cy.get('#ContactUsFrm_email').type("test@wp.pl");
         cy.get('#ContactUsFrm_enquiry').type("What is your favorite framework?") 
         cy.get("button[title='Submit']").click();
+        cy.get('.mb40 > :nth-child(3)').should("have.text","Your enquiry has been successfully sent to the store owner!")
     })
-    it.only("Automation test for contact us form with Xpath selectors",()=>{
+    it("Automation test for contact us form with Xpath selectors",()=>{
         cy.visit("https://www.automationteststore.com/");
         cy.xpath("//a[contains(@href,'contact')]").click();
-        // cy.get('#ContactUsFrm_first_name').type("Szymon");
-        // cy.get('#ContactUsFrm_email').type("test@wp.pl");
-        // cy.get('#ContactUsFrm_enquiry').type("What is your favorite framework?") 
-        // cy.get("button[title='Submit']").click();
+       
     })
 })
