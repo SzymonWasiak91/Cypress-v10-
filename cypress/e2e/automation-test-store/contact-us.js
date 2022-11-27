e291f98879499f09edc167f4d728fab381096247
Szymon <wasiak.szymon.1991@gmail.com>
2022-11-27 17:18:50 +0100

    Add assertions

--- cypress/e2e/automation-test-store/contact-us.js
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -7,6 +7,7 @@ describe("Automation test store",()=>{
         cy.get("a[href$='contact']").click();
         cy.get('#ContactUsFrm_first_name').type("Szymon");
         cy.get('#ContactUsFrm_email').type("test@wp.pl");
+        cy.get('#ContactUsFrm_email').should("have.attr","name", "email");
         cy.get('#ContactUsFrm_enquiry').type("What is your favorite framework?") 
         cy.get("button[title='Submit']").click();
         cy.get('.mb40 > :nth-child(3)').should("have.text","Your enquiry has been successfully sent to the store owner!")
--- cypress/e2e/webdriver-uni/contact.js
+++ cypress/e2e/webdriver-uni/contact.js
@@ -2,23 +2,25 @@
 
 describe("Test contact us page / form", ()=>{
     it("Should be able to submit a succesfull submission contact us form - possitive TC", ()=>{
-        //cypress code
-        // cy.visit("http://www.webdriveruniversity.com/")
-        // cy.get("#contact-us").click()
         cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
         cy.get("input[name='first_name']").type("Szymon");
         cy.get("input[name='last_name']").type("Tester");
         cy.get("input[name='email']").type("test@wp.pl");
         cy.get("[name='message']").type("Some example text");
         cy.get("[value='SUBMIT']").click();
+        cy.get('h1').should("have.text", "Thank You for your Message!");
     })
     it("Should not be able to send contac us form page - negative TC", ()=>{
         cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+        cy.url().should("include","/Contact-Us/contactus.html");
+        cy.title().should("include","WebDriver | Contact Us");
         cy.get("input[name='first_name']").type("Michal");
         cy.get("input[name='last_name']").type("Programista");
         cy.get("input[name='email']").type(" ");
         cy.get("[name='message']").type("Some example comment content");
         cy.get("[value='SUBMIT']").click();
+        cy.get('body').contains("Error: Invalid email address");
+        cy.document().should("have.property","charset").and("eq","UTF-8")
         
     })
 } )
