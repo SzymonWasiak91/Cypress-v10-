59cab7e47832cd3418ef815f7eaf2235e3947443
Szymon <wasiak.szymon.1991@gmail.com>
2022-11-27 13:51:03 +0100

    Add new folder with tests

---
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -0,0 +1,12 @@
+/// <reference types="Cypress" />
+
+describe("Automation test store",()=>{
+    it("Automation test for contact us form",()=>{
+        cy.visit("https://www.automationteststore.com/");
+        cy.get("a[href='https://automationteststore.com/index.php?rt=content/contact']").click();
+        cy.get('#ContactUsFrm_first_name').type("Szymon");
+        cy.get('#ContactUsFrm_email').type("test@wp.pl");
+        cy.get('#ContactUsFrm_enquiry').type("What is your favorite framework?") 
+        cy.get("button[title='Submit']").click();
+    })
+})
